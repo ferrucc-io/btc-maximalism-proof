6978ad41e7c83e8409929ab91d244c3d5d9479e6
ferrucc-io <hi@ferrucc.io>
2019-06-14 01:04:26 +0200

    Start filling in some details in DB

--- src/cljs/btc_maximalism_proof/db.cljs
+++ src/cljs/btc_maximalism_proof/db.cljs
@@ -5,4 +5,136 @@
                  :title "Hard vs Easy"
                  :description "Money that is hard to create and censor is hard to come by. There’s already a lot of money that is easy to censor and easy to create, no need for other alternatives. The alternative cannot be diluted or censored. Differences must be kept."
                  :parent-a 0
-                 :parent-b 0}]})
+                 :parent-b 0}
+                {:id 2
+                 :title "Coinbase & Blockchain"
+                 :description "There’s nothing new in bitcoin, most of them are legacy. Only two new things are coinbase process, fixed monetary policy and blockchain. Blockchain is not a silver bullet, it makes systems worse, slower and less privacy friendly."
+                 :parent-a 0
+                 :parent-b 0}
+
+                {:id 3
+                 :title "Cloning Theorem"
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 4
+                 :title "Top VS Bottom"
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 5
+                 :title "Immaculate Conception"
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 6
+                 :title "Scam Definition"
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 7
+                 :title "Simplification Proviso"
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 8
+                 :title "Populist Theory"
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 9
+                 :title "Social Layer"
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 10
+                 :title "Inner Regulator"
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 11
+                 :title "Blockchain Fallacy"
+                 :description ""
+                 :parent-a 1
+                 :parent-b 2}
+                {:id 12
+                 :title "Supply Inflation"
+                 :description ""
+                 :parent-a 1
+                 :parent-b 3}
+                {:id 13
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 14
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 15
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 16
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 17
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                {:id 18
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+ {:id 19
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+ {:id 20
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+ {:id 21
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+ {:id 22
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+ {:id 23
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+ {:id 24
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+ {:id 25
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+ {:id 26
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+ {:id 27
+                 :title ""
+                 :description ""
+                 :parent-a 0
+                 :parent-b 0}
+                ]})
